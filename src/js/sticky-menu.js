841b712575c24ae093b448b3b5eb9a376428c651
Surfing-Chef <surfing-chef@surfing-chef.com>
2018-03-17 19:20:07 -0700

    Started coding sticky top menu on scroll

---
+++ src/js/sticky-menu.js
@@ -0,0 +1,45 @@
+// /**
+//  * File sticky-menu.js.
+//  *
+//  * Enable top menu to stick to top of view port when scrolled off the screen
+//  *
+//  * Contains handlers to make Theme Customizer preview reload changes asynchronously.
+//  */
+
+// $( document ).ready(function() {
+
+//     var header_wrap, header, icons, site_nav;
+
+// 	header_wrap = $('.header-wrapper');
+// 	if ( ! header_wrap ) {
+// 		return;
+//     }
+    
+//     header = $('#masthead');
+// 	if ( ! header ) {
+// 		return;
+//     }
+
+//     icons = $('.social-icons');
+// 	if ( ! icons ) {
+// 		return;
+//     }
+
+//     site_nav = $('#site-navigation');
+// 	if ( ! site_nav ) {
+// 		return;
+//     }
+
+//     // console.log( $(window).height() );
+
+//         // var wScroll = $(this).scrollTop();
+//         // var hHeight = header.height();
+    
+//         // if wScroll > hHeight {
+//         //     console.log('Off Screen');
+//         // } else {
+//     $(window).scroll(function(e) {
+//         console.log( $(window).height() );
+//     });
+//     //   });
+// });
