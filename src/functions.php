e2dbdf9099ce2e4dc2fe7391b70a2bd4af762059
Surfing-Chef <surfing-chef@surfing-chef.com>
2018-03-17 17:19:57 -0700

    Added function to display breadcrumb in header

--- src/functions.php
+++ src/functions.php
@@ -185,3 +185,58 @@ if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
 
+/**
+ * Display breadcrumb where function is called.
+ * 
+ * @link https://www.thatweblook.co.uk/advice/tutorial-wordpress-breadcrumb-function/
+ */
+function the_breadcrumb() {
+
+    $sep = ' &nbsp&nbsp&nbsp&nbsp ';
+
+    if (!is_front_page()) {
+
+        echo '<div class="breadcrumbs">';
+        echo '<a href="';
+        echo get_option('home');
+        echo '">';
+        bloginfo('name');
+        echo '</a>' . $sep;
+
+        if (is_category() || is_single() ){
+            the_category('title_li=');
+        } elseif (is_archive() || is_single()){
+            if ( is_day() ) {
+                printf( __( '%s', 'text_domain' ), get_the_date() );
+            } elseif ( is_month() ) {
+                printf( __( '%s', 'text_domain' ), get_the_date( _x( 'F Y', 'monthly archives date format', 'text_domain' ) ) );
+            } elseif ( is_year() ) {
+                printf( __( '%s', 'text_domain' ), get_the_date( _x( 'Y', 'yearly archives date format', 'text_domain' ) ) );
+            } else {
+                _e( 'Blog Archives', 'text_domain' );
+            }
+        }
+
+        if (is_single()) {
+            echo $sep;
+            the_title();
+        }
+
+        if (is_page()) {
+            echo the_title();
+        }
+
+        if (is_home()){
+            global $post;
+            $page_for_posts_id = get_option('page_for_posts');
+            if ( $page_for_posts_id ) { 
+                $post = get_page($page_for_posts_id);
+                setup_postdata($post);
+                the_title();
+                rewind_posts();
+            }
+        }
+
+        echo '</div>';
+    }
+}
