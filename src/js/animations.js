e36cb11ad090da7011a7018549c3be2824149646
Surfing Chef <surfing-chef@surfing-chef.com>
2018-03-31 00:25:26 -0700

    Used jquery and css transitions on nav search button

--- src/js/animations.js
+++ src/js/animations.js
@@ -1,11 +1,28 @@
 ( function( $ ) {
     $( document ).ready(function() {
-        $( '.menu-search' ).toggleClass( 'invisible' );
 
         $( '.menu-search-button' ).click(function(){
-            $( '.menu-search' ).toggleClass( 'invisible' );
-            $( '.menu-search' ).toggleClass( 'animated slideInRight' );
+            // if has class clicked
+            if ( !$(this).hasClass('clicked') ){
+                $(this).toggleClass( 'clicked' );
+                $( '.menu-search' ).css({   
+                                            'opacity': '1',
+                                            'background-color': '#fff',
+                                            'transform': 'translateX(1px)',
+                                            'visibility': 'visible'
+                                        });
+            }
+            else if ( $(this).hasClass('clicked') ){
+                $(this).toggleClass( 'clicked' );
+                $( '.menu-search' ).css({   
+                                            'visibility': 'hidden',
+                                            'background-color': 'beige',
+                                            'opacity': '0',
+                                            'transform': 'translateX(110%)'
+                                        });
+            }
         });
+        
     });
 } )( jQuery );
 
